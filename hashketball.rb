c0bd552093b9798a898772ef01e70971a321454f
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-04 03:43:33 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,31 +15,103 @@ def game_hash
             :steals => "3",
             :blocks => "1",
             :slam_dunks => "1"
+        },
+         "Reggie Evans" => {
+            :number => "30",
+            :shoe => "14",
+            :points => "12",
+            :rebounds => "12",
+            :assists => "12",
+            :steals => "12",
+            :blocks => "12",
+            :slam_dunks => "7"
+        },
+         "Brook Lopez" => {
+            :number => "11",
+            :shoe => "17",
+            :points => "17",
+            :rebounds => "19",
+            :assists => "10",
+            :steals => "3",
+            :blocks => "1",
+            :slam_dunks => "15"
+        },
+         "Mason Plumlee" => {
+            :number => "1",
+            :shoe => "19",
+            :points => "26",
+            :rebounds => "12",
+            :assists => "6",
+            :steals => "3",
+            :blocks => "8",
+            :slam_dunks => "5"
+        },
+         "Jason Terry" => {
+            :number => "31",
+            :shoe => "15",
+            :points => "19",
+            :rebounds => "2",
+            :assists => "2",
+            :steals => "4",
+            :blocks => "11",
+            :slam_dunks => "1"
         }
-      }
-      {
-        # :number => ["0", "30", "11", "1", "31"],
-        # :shoe => ["16", "14", "17", "19", "15"],
-        # :points => ["22", "12", "17", "26", "19"],
-        # :rebounds => ["12", "12", "19", "12", "2"],
-        # :assists => ["12", "12", "10", "6", "2"],
-        # :steals => ["3", "12", "3", "3", "4"],
-        # :blocks => ["1", "12", "1", "8", "11"],
-        # :slam_dunks => ["1", "7", "15", "5", "1"]
       }
     },
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => {
-        :number => ["4", "0", "2", "8", "33"],
-        :shoe => ["18", "16", "14", "15", "15"],
-        :points => ["10", "12", "24", "33", "6"],
-        :rebounds => ["1", "4", "12", "3", "12"],
-        :assists => ["1", "7", "12", "2", "12"],
-        :steals => ["2", "7", "4", "1", "22"],
-        :blocks => ["7", "15", "5", "1", "5"],
-        :slam_dunks => ["2", "10", "5", "0", "12"],
+        "Jeff Adrien" => {
+            :number => "4",
+            :shoe => "18",
+            :points => "10",
+            :rebounds => "1",
+            :assists => "1",
+            :steals => "2",
+            :blocks => "7",
+            :slam_dunks => "2"
+        },
+         "Bismak Biyombo" => {
+            :number => "0",
+            :shoe => "16",
+            :points => "12",
+            :rebounds => "4",
+            :assists => "7",
+            :steals => "7",
+            :blocks => "15",
+            :slam_dunks => "10"
+        },
+         "DeSagna Diop" => {
+            :number => "2",
+            :shoe => "14",
+            :points => "24",
+            :rebounds => "12",
+            :assists => "12",
+            :steals => "4",
+            :blocks => "5",
+            :slam_dunks => "5"
+        },
+         "Ben Gordon" => {
+            :number => "8",
+            :shoe => "15",
+            :points => "33",
+            :rebounds => "3",
+            :assists => "2",
+            :steals => "1",
+            :blocks => "1",
+            :slam_dunks => "0"
+        },
+         "Brendan Haywood" => {
+            :number => "33",
+            :shoe => "15",
+            :points => "6",
+            :rebounds => "12",
+            :assists => "12",
+            :steals => "22",
+            :blocks => "5",
+            :slam_dunks => "12"
+        }
       }
     }
   }
