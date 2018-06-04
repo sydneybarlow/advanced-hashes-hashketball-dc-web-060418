dc791ce2b151ce4c24adad351820cbca678b932c
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-04 03:34:15 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,15 +6,15 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => { 
-        "Jeff Adrian" => {
-            :number
-            :shoe
-            :points
-            :rebounds
-            :assists
-            :steals
-            :blocks
-            :slam_dunks
+        "Alan Anderson" => {
+            :number => "0",
+            :shoe => "16",
+            :points => "22",
+            :rebounds => "12",
+            :assists => "12",
+            :steals => "3",
+            :blocks => "1",
+            :slam_dunks => "1"
         }
       }
       {
