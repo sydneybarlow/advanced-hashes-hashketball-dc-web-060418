a12325036317a506cb64e0f57a3fdb2c33656e0c
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-04 03:51:54 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,11 +130,12 @@ def good_practices
 end
 
 def num_points_scored(players_name)
+  players_name = []
   game_hash.each do |location, team_data|
     team_data.each do |info, players|
       players.each do |points|
         if points == :points
-          return
+          players_name.push(points)
       end  
     end  
   end  
